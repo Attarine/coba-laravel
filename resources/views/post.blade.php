5fd25d41ecb67db3cc1ea62498eecd0cf5e07527
Attarine <attarinecicioramadhan@gmail.com>
2022-01-22 12:22:46 +0700

    Update post.blade.php

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,15 +3,11 @@
 @section('container')
 
 <article>
-    <div class="card" style="width: 100%;">
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-            <h2 class="card-title">{{ $post["title"] }}</h2>
-            <h5 class="card-text">By : {{ $post["author"] }}</h5>
-            <p class="card-text">{{ $post["body"] }}</p> 
-        </div>
-      </div>
+    <h2>{{ $post["title"] }}</h2>
+    <h5>{{ $post["author"] }}</h5>
+    <p>{{ $post["body"] }}</p>
 </article>
-<a href="/blog" class="btn btn-warning mt-2">Back To Post</a>
+
+<a href="/blog">Back To Post</a>
 
 @endsection
